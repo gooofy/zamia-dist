667e5b0fe128c5a95c959e1ff06208596fc06bb2
guenter <guenter@zamia.org>
2018-07-06 12:20:15 +0200

    python-kaldiasr buildscripts added

---
+++ raspbian-ai/python-kaldiasr/doit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+VERSION=0.4.0
+PKGNAME=python-kaldiasr
+GITNAME=py-kaldi-asr
+export DEBFULLNAME="Guenter Bartsch"
+
+./clean.sh
+
+cp -r ${GITNAME} ${PKGNAME}-${VERSION}
+pushd ${PKGNAME}-${VERSION}
+rm -rf .git
+
+make clean
+
+# dh_make -c apache -e 'guenter@zamia.org' --python --createorig
+
+cp -r ../debian .
+
+debuild -b -uc -us
+
