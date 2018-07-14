04451afe5d55d4a4e438a202de41714704c83615
Guenter Bartsch <guenter@zamia.org>
2018-07-14 23:11:16 +0200

    python-nltools buildfiles added

---
+++ centos-ai/python-nltools/doit.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+VERSION=0.3.0
+
+rm -rf py-nltools-${VERSION} py-nltools-${VERSION}.tar.gz
+
+pushd py-nltools
+make clean README.md
+popd
+
+cp -r py-nltools py-nltools-${VERSION}
+tar cfvz py-nltools-${VERSION}.tar.gz py-nltools-${VERSION}
+
+rm -rf py-nltools-${VERSION}
+
+rm -rf ~/rpmbuild
+mkdir -p ~/rpmbuild/SOURCES
+cp * ~/rpmbuild/SOURCES
+
+rpmbuild -ba --clean python-nltools.spec
+
+rm -f py-nltools-${VERSION}.tar.gz
+
