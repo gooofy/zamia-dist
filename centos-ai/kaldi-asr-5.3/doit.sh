a19d7f6e6f8dcaf64f5429bcced82a69fd95c0d6
Guenter Bartsch <guenter@zamia.org>
2018-07-23 23:42:41 +0200

    remove old kaldi 5.3 buildfiles

--- centos-ai/kaldi-asr-5.3/doit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -rf ~/rpmbuild
-mkdir -p ~/rpmbuild/SOURCES
-cp * ~/rpmbuild/SOURCES
-
-rpmbuild -bb --clean kaldi-asr.spec
-
--- centos-ai/kaldi-asr-5.3/take.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp ~/rpmbuild/RPMS/x86_64/*.rpm ~/repo/ai/centos/7/x86_64
-cp ~/rpmbuild/RPMS/noarch/*.rpm ~/repo/ai/centos/7/x86_64
-createrepo ~/repo/ai/centos/7/x86_64
-echo rsync --delete -avzP ~/repo/ai/ guenter@goofy.zamia.org:/var/www/html/repo-ai/
