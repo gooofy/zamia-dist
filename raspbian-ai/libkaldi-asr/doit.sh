2565e1a77b7c833e65c4e87955025f03c5054c04
Guenter Bartsch <guenter@zamia.org>
2019-05-05 17:36:13 +0200

    build raspbian libkaldi-asr packages from source just like the debian ones

--- raspbian-ai/libkaldi-asr/doit.sh
+++ raspbian-ai/libkaldi-asr/doit.sh
@@ -1,49 +1,26 @@
 #!/bin/bash
 
-VERSION="5.3-1"
+VERSION="5.4.248"
+GITNAME=kaldi
+PKGNAME=libkaldi-asr
+export DEBFULLNAME="Guenter Bartsch"
 
-TARBALLNAME=kaldi-asr-bin.tar.gz
+./clean.sh
 
-echo "tar..."
+# pushd ${GITNAME}
+# make clean
+# make README.md
+# popd
 
-pushd /opt
-tar cfvz /tmp/${TARBALLNAME} kaldi/tools/openfst kaldi/tools/openfst-1.6.5/lib/*.so* kaldi/tools/openfst-1.6.5/include  kaldi/src/*/*.h kaldi/src/*/*.so kaldi/COPYING kaldi/INSTALL kaldi/README.md kaldi/src/INSTALL kaldi/src/NOTES kaldi/src/TODO 
+cp -r ${GITNAME} ${PKGNAME}-${VERSION}
+pushd ${PKGNAME}-${VERSION}
+rm -rf .git
+
+cp -r ../debian .
+# cp ../linux_atlas.mk src/makefiles/
+
+# dh_make -c lgpl3 -e 'guenter@zamia.org' --python --createorig
+debuild -b -uc -us
+
 popd
-
-rm -rf libkaldi-asr
-mkdir -p libkaldi-asr/opt
-
-tar xfvz /tmp/${TARBALLNAME} -C libkaldi-asr/opt/
-
-find libkaldi-asr/opt -name "*.so*" -type f -print -exec chmod 644 \{\} \;
-find libkaldi-asr/opt -name "*.so*" -type f -print -exec strip \{\} \;
-find libkaldi-asr/opt -name "*.so*" -type f -print -exec chrpath -d \{\} \;
-
-# ld
-
-mkdir -p libkaldi-asr/etc/ld.so.conf.d
-cp 99-kaldi.conf libkaldi-asr/etc/ld.so.conf.d/
-
-# pkg-config
-
-mkdir -p libkaldi-asr/usr/lib/pkgconfig
-cp kaldi-asr.pc libkaldi-asr/usr/lib/pkgconfig/
-
-# DEBIAN
-
-mkdir libkaldi-asr/DEBIAN
-sed "s/VERSION/${VERSION}/g" DEBIAN/control >libkaldi-asr/DEBIAN/control
-cp DEBIAN/postinst libkaldi-asr/DEBIAN/
-
-# built .deb
-
-echo "dpkg-deb..."
-
-fakeroot dpkg-deb --build libkaldi-asr
-
-mv libkaldi-asr.deb libkaldi-asr_${VERSION}_armhf.deb
-
-# run lintian on it
-
-lintian libkaldi-asr_${VERSION}_armhf.deb | tee lr.txt
 
--- raspbian-ai/libkaldi-asr/take.sh
+++ raspbian-ai/libkaldi-asr/take.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-REPO=${HOME}/repo/ai/raspbian/stretch/armhf
+REPO=${HOME}/repo/ai/raspbian/stretch/armhf/
 
 rm -f ${REPO}/libkaldi-asr*.deb
 
+rm libkaldi-asr-dbgsym_*.deb
 cp libkaldi-asr*.deb ${REPO}
 
 pushd ${REPO}
-rm Packages.gz
-dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 ./update.sh
 popd
 
