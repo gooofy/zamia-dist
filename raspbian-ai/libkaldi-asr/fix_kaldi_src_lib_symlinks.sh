a74964556c75780194493015f02e72a0d8364c64
Guenter Bartsch <guenter@zamia.org>
2019-06-02 13:27:13 +0200

    fix symlinks

--- raspbian-ai/libkaldi-asr/fix_kaldi_src_lib_symlinks.sh
+++ raspbian-ai/libkaldi-asr/fix_kaldi_src_lib_symlinks.sh
@@ -3,7 +3,7 @@
 cd opt/kaldi/src/lib 
 
 for i in *.so ; do 
-    NEWLNK=`readlink $i | sed 's/\/home\/bofh\/build\/debian-ai\/libkaldi-asr\/libkaldi-asr-.......\/src/../g'` 
+    NEWLNK=`readlink $i | sed 's/\/home\/bofh\/build\/raspbian-ai\/libkaldi-asr\/libkaldi-asr-.......\/src/../g'` 
     echo $NEWLNK 
     rm $i
     ln -s $NEWLNK $i
